0fc82108920ba405359953f4eada9fd7cad3cb9e
ferlazo05 <fernando.lazo0509@gmail.com>
2022-06-20 18:29:48 -0600

    Add project files.

---
+++ Ejercicio1_2/Ejercicio1_2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btninformacion_clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtnombre.Text) && !string.IsNullOrEmpty(txtapellido.Text) && !string.IsNullOrEmpty(txtedad.Text) && !string.IsNullOrEmpty(txtcorreo.Text))
+            {
+                var Usuario = new Models.Usuario();
+                Usuario.nombre = txtnombre.Text;
+                Usuario.apellido = txtapellido.Text;
+                Usuario.edad = txtedad.Text;
+                Usuario.correo = txtcorreo.Text;
+                SecondPage page = new SecondPage();
+                page.BindingContext = Usuario;
+                await Navigation.PushAsync(page);
+            }
+            else
+            {
+                DisplayAlert("Aviso", "Necesita llenar los campos.", "Ok");
+            }
+        }
+    }
+}
---
+++ Ejercicio1_2/Ejercicio1_2/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_2.Models
+{
+    public class Usuario
+    {
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public String edad { get; set; }
+        public String correo { get; set; }
+    }
+}
